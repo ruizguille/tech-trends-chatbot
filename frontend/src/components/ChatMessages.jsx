0200279945a9f2ff6e59de444b7625efadb250b0
ruizguille <ruiz.ruiz.guillermo@gmail.com>
2024-08-16 13:31:52 +0200

    Add markdown to frontend & other updates

--- frontend/src/components/ChatMessages.jsx
+++ frontend/src/components/ChatMessages.jsx
@@ -1,7 +1,8 @@
+import Markdown from 'react-markdown';
 import useAutoScroll from '@/hooks/useAutoScroll';
 import Spinner from '@/components/Spinner';
-import assistantIcon from '@/assets/images/assistant.svg';
 import userIcon from '@/assets/images/user.svg';
+import errorIcon from '@/assets/images/error.svg';
 
 function ChatMessages({ messages, isLoading }) {
   const scrollContentRef = useAutoScroll(isLoading);
@@ -16,19 +17,22 @@ function ChatMessages({ messages, isLoading }) {
           {role === 'user' && (
             <img
               className='h-[26px] w-[26px] shrink-0'
-              src={role === 'user' ? userIcon : assistantIcon}
+              src={userIcon}
               alt='user'
             />
           )}
           <div>
             <div className='markdown-container'>
               {(loading && !content) ? <Spinner />
-                : <div className='whitespace-pre-line'>{content}</div>
+                : (role === 'assistant')
+                  ? <Markdown>{content}</Markdown>
+                  : <div className='whitespace-pre-line'>{content}</div>
               }
             </div>
             {error && (
               <div className={`flex items-center gap-1 text-sm text-error-red ${content && 'mt-2'}`}>
-                Error generating the response
+                <img className='h-5 w-5' src={errorIcon} alt='error' />
+                <span>Error generating the response</span>
               </div>
             )}
           </div>
