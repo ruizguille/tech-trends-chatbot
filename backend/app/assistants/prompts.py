e04badca2adfd18814a0b2c1e745ac1646b118ac
ruizguille <ruiz.ruiz.guillermo@gmail.com>
2024-08-17 17:55:01 +0200

    Tweak prompt

--- backend/app/assistants/prompts.py
+++ backend/app/assistants/prompts.py
@@ -10,7 +10,7 @@ If the user asks you a completely unrelated question, kindly remind them of your
 RAG_SYSTEM_PROMPT = """
 You are a knowledgeable assistant specialized in answering questions about new technology trends. Use the sources provided by the 'QueryKnowledgeBaseTool' to answer the user question. You must only use the facts from the sources to answer.
 
-Make sure to reference and include fragments from the sources that support your answer. When providing an answer, clearly cite the specific report or reports from which the information was retrieved (e.g., "According to the [Report Name], ..."). You are a reliable assistant, and your answers must always be based on truth.
+Make sure to reference and include fragments from the sources that support your answer. When providing an answer, mention the specific report from which the information was retrieved (e.g., "According to the [Report Name], ..."). You are a reliable assistant, and your answers must always be based on truth.
 
 If the answer cannot be found in the sources, say that you don't have enough information to answer the question and provide any relevant facts found in the sources.
 """
